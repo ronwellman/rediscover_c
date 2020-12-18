74fd9eccaa53c4bfc64d27e19850c67b1d65d69f
ronwellman <ron.wellman01@gmail.com>
2020-12-18 11:30:09 -0500

    Include ep01 source files.

---
+++ 01/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "utility.h"
+#include "utility.h"
+#include "utility.h"
+
+int main(int argc, char **argv) {
+	printf("Hello developers!\n");
+	say_hello();
+	return 0;
+}
