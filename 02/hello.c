6b0d1499a00e7c14d2a0de0dc470f9ef225bafd0
ronwellman <ron.wellman01@gmail.com>
2020-12-18 13:47:45 -0500

    Copy over hello program from ep01.

---
+++ 02/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "utility.h"
+
+int main(int argc, char **argv) {
+	printf("Hello developers!\n");
+	say_hello();
+	return 0;
+}
---
+++ 02/utility.h
@@ -0,0 +1,6 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+void say_hello(void);
+
+#endif
