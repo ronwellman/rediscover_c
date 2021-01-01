043cc863e10e1fd26770173f85748f94e75587fb
ronwellman <ron.wellman01@gmail.com>
2021-01-01 14:23:21 -0500

    09 - reference.

---
+++ 09/README.md
@@ -0,0 +1,8 @@
+# Pass by value vs Pass by reference
+* [tutorialspoint](https://www.tutorialspoint.com/cprogramming/c_function_call_by_reference.htm)
+* [wikipedia.org](https://en.wikipedia.org/wiki/Stack-based_memory_allocation)
+
+Often times you'll need to pass the address of something rather than the thing itself
+* more memory efficient
+* you want to update the thing in the function
+* you may not know how many elements an array has during compilation
---
+++ 09/printarray.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+void printarray(int *, int);
+int sum(int *, int, int *);
+
+int main() {
+	int numbers[] = { 1,2,3,4,5,6,7,8,9 };
+	int primes[] = { 2,3,5,7 };
+	int *garbage = NULL;
+	int status;
+	int val; 
+
+	printf("elements numbers: %ld\n", sizeof(numbers) / sizeof(int));
+	printf("elements primes: %ld\n", sizeof(primes) / sizeof(int));
+
+	printarray(numbers, sizeof(numbers) / sizeof(int));
+	printarray(primes, sizeof(primes) / sizeof(int));
+	printarray(garbage, 2);
+	
+	status = sum(numbers, sizeof(numbers) / sizeof(int), &val);
+	printf("Status: %d\n", status);
+	printf("Sum: %d\n", val);
+	status = sum(primes, sizeof(primes) / sizeof(int), &val);
+	printf("Status: %d\n", status);
+	printf("Sum: %d\n", val);
+
+	status = sum(garbage, sizeof(primes) / sizeof(int), &val);
+	printf("Status: %d\n", status);
+	printf("Sum: %d\n", val);
+
+
+	return 0;
+}
+
+void printarray(int array[], int elements) {
+	if (NULL == array) {
+		fprintf(stderr, "array is NULL\n");
+		return;
+	}
+
+	for(int i = 0; i < elements; i++) {
+		printf("%d ", array[i]);
+	}
+
+	printf("\n");
+}
+
+int sum(int array[], int elements, int *val) {
+	if (NULL == array || NULL == val) {
+		fprintf(stderr, "pointer is NULL\n");
+		return -1;
+	}
+
+	*val = 0;
+
+	for(int i = 0; i < elements; i++) {
+		*val += array[i];
+	}
+
+	return 0;
+}
